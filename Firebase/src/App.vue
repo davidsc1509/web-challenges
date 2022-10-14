24e83bbbf40cebd6331eb460e9da332ecd0fd32a
davidsc1509 <david.sanchez_col@uao.edu.co>
2022-10-13 22:30:58 -0500

    Entregas segundo corte

---
+++ Firebase/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <Iniciosesion />
+</template>
+
+<script>
+import Iniciosesion from './components/iniciosesion.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Iniciosesion
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Firebase/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import store from './store/user/Index'
+import router from './routes'
+
+createApp(App)
+    .use(store)
+    .use(router)
+    .mount('#app')
